8bc5a890d8f3fecfca68dbf20d52441a0091e01e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-12 03:14:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,42 +3,37 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 const session = require('express-session');
-const RedisStore = require('connect-redis');
-const redis = require('redis');
-const redisClient = redis.createClient();
 
 const app = express();
 const PORT = 3000;
 
 
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir);
-}
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(session({
-    store: new RedisStore({ client: redisClient }), 
     secret: 'ab', 
     resave: false,
     saveUninitialized: true,
 }));
+
 
 const upload = multer({
     dest: 'uploads/',
     limits: { fileSize: 5 * 1024 * 1024 }, 
 });
 
+
 const uploads = [];
 
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.post('/admin-login', (req, res) => {
     const { username, password } = req.body;
 
+    
     if (username === 'admin' && password === 'abbot') {
         req.session.isAdmin = true;
         res.status(200).json({ message: 'Login successful!' });
@@ -46,6 +41,7 @@ app.post('/admin-login', (req, res) => {
         res.status(401).json({ message: 'Invalid credentials' });
     }
 });
+
 
 function isAdmin(req, res, next) {
     if (req.session.isAdmin) {
@@ -55,6 +51,7 @@ function isAdmin(req, res, next) {
     }
 }
 
+
 app.get('/uploads-data', isAdmin, (req, res) => {
     res.status(200).json(uploads);
 });
@@ -62,6 +59,7 @@ app.get('/uploads-data', isAdmin, (req, res) => {
 app.get('/customer.html', isAdmin, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'customer.html'));
 });
+
 
 app.post('/upload', upload.single('image'), (req, res) => {
     const text = req.body.text;
@@ -71,19 +69,14 @@ app.post('/upload', upload.single('image'), (req, res) => {
         return res.status(400).json({ message: 'Text and image are required.' });
     }
 
-   
-    const validMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-    if (!validMimeTypes.includes(imageFile.mimetype)) {
-        return res.status(400).json({ message: 'Invalid image type. Only JPEG, PNG, and GIF are allowed.' });
-    }
-
+    
     const targetPath = path.join(__dirname, 'uploads', imageFile.originalname);
     fs.rename(imageFile.path, targetPath, (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error saving the image file.' });
         }
 
-      
+       
         const uploadData = {
             text,
             imageUrl: `/uploads/${imageFile.originalname}`,
@@ -100,7 +93,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
